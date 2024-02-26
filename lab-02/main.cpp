77f8270abb32d29fb47411f76f2b01147873e751
sup1337 <64952429+sup1337@users.noreply.github.com>
2024-02-26 18:12:43 +0200

    teszt

--- lab-02/main.cpp
+++ lab-02/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void nagyobbHatvanyKiirasa(double szam1, double szam2) {
+    double hatvany1 = pow(szam1, szam2);
+    double hatvany2 = pow(szam2, szam1);
+
+    double nagyobbHatvany = (hatvany1 > hatvany2) ? hatvany1 : hatvany2;
+
+    cout << "A nagyobb " << nagyobbHatvany << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double szam1, szam2;
+    cout << "elso szamot: ";
+    cin >> szam1;
+    cout << "masodik szamot: ";
+    cin >> szam2;
+
+    nagyobbHatvanyKiirasa(szam1, szam2);
+
     return 0;
 }
